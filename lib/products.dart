cebb81ea2bfce3738f16eadf226241bd1dd57beb
santeala000 <alansantes202@gmail.com>
2020-09-30 10:58:15 -0400

    edited products.dart

--- lib/products.dart
+++ lib/products.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+
+class Products extends StatefulWidget {
+  @override
+  _ProductsState createState() => _ProductsState();
+}
+
+class _ProductsState extends State<Products> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[600],
+        title: Text("Products"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+    );
+  }
+}
