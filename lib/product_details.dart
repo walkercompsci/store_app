c0c1ef18eb5a95cc5e28f799118c2298612a0194
santeala000 <alansantes202@gmail.com>
2020-10-27 10:53:35 -0400

    added products.dart

---
+++ lib/product_details.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+//class getProducts {
+  //final
+//}
+
+class ProductDetails extends StatefulWidget {
+  //final product_detail_name;
+
+  @override
+  _ProductDetailsState createState() => _ProductDetailsState();
+}
+
+class _ProductDetailsState extends State<ProductDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
