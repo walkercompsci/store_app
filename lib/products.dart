21ebdca8be7a90c75ce85eb50faf51340a21a5e5
mcculeth000 <mcculeth000@warren.k12.in.us>
2020-11-19 10:55:24 -0500

    Added to begin

--- lib/products.dart
+++ lib/products.dart
@@ -11,32 +11,63 @@ class _ProductsState extends State<Products> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.1,
-        backgroundColor: Colors.red,
-        title: Text('Products'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            color: Colors.white,
-            onPressed: () {},
+      body: SafeArea(
+        child: Column(
+        children: [
+          AppBar(
+            backgroundColor: Colors.purple[300],
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton.icon(onPressed: () {
+                  Navigator.pushNamed(context, '/mission');
+                },
+                  icon: Icon(Icons.airport_shuttle),
+                  label: Text("Mission"),
+                ),
+                FlatButton.icon(onPressed: () {
+                  Navigator.pushNamed(context, '/contacts');
+                },
+                  icon: Icon(Icons.account_circle_rounded),
+                  label: Text("Contacts"),
+                ),
+                FlatButton.icon(onPressed: () {
+                  Navigator.pushNamed(context, '/products');
+                },
+                  icon: Icon(Icons.add_a_photo),
+                  label: Text("Community"),
+                ),
+                Icon(Icons.wb_sunny),
+                SizedBox(width: 10),
+                Text("Products"),
+                SizedBox(width: 10,),
+              ],
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.shopping_cart),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          ListView(
+            shrinkWrap: true,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+//                padding: EdgeInsets.only(top: 8.0),
+//                margin: EdgeInsets.only(top:10.0),
+                  height: MediaQuery.of(context).size.height,
+                  child: ProductsDetails(),
+                  //test
+                ),
+              ),
+            ],
           ),
         ],
       ),
-      body: ListView(
-        shrinkWrap: true,
-        children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-//                padding: EdgeInsets.only(top: 8.0),
-//                margin: EdgeInsets.only(top:10.0),
-                height: MediaQuery.of(context).size.height,
-                child: ProductsDetails(),
-                //test
-              ),
-            ),
-        ],
       ),
     );
   }
